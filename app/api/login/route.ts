3dd68a260d59ae99c7e655e610d30edec3e03e5f
ntoiti <fidesluckia36@gmail.com>
2025-10-09 14:11:39 +0300

    added code

---
+++ app/api/login/route.ts
@@ -0,0 +1,37 @@
+import {NextResponse } from 'next/server';
+import { connectDB } from '@/lib/mongodb';
+import User from '@/models/user';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+export async function POST(req:Request){
+    const{email,password}=await req.json();
+
+    if(!email ||!password){
+        return NextResponse.json({message:'Email and Password are required'},{status:400});
+    }
+    await connectDB(); 
+    try{
+        const user=await User.find({email}).lean()
+        if(!user){
+            return NextResponse.json({message:'Invalid email or password'},{status:400})
+        }
+        const isMatch=await bcrypt.compare(password,user[0].password);
+        if(!isMatch){
+            return NextResponse.json({message:'Invalid email or password'},{status:400})
+        }
+
+        const token=jwt.sign(
+            {id:user[0]._id,
+                email:user[0].email,
+            },
+            process.env.JWT_SECRET||'secret',
+            {expiresIn:'1d'
+            }
+        )
+        return NextResponse.json({message:'Login successful',user:{id:user[0]._id,fullname:user[0].fullname,email:user[0].email}},{status:200})
+
+    } catch(error){
+        return NextResponse.json({message:'An error occurred, please try again'},{status:500})
+    }   
+          
+}    
