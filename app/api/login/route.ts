7b7c7ea01f326dabcc64dba471462ce63de237a9
ntoiti <fidesluckia36@gmail.com>
2025-10-09 14:42:46 +0300

    commented code

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,7 +2,7 @@ import {NextResponse } from 'next/server';
 import { connectDB } from '@/lib/mongodb';
 import User from '@/models/user';
 import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken'
+// import jwt from 'jsonwebtoken'
 export async function POST(req:Request){
     const{email,password}=await req.json();
 
@@ -20,17 +20,18 @@ export async function POST(req:Request){
             return NextResponse.json({message:'Invalid email or password'},{status:400})
         }
 
-        const token=jwt.sign(
-            {id:user[0]._id,
-                email:user[0].email,
-            },
-            process.env.JWT_SECRET||'secret',
-            {expiresIn:'1d'
-            }
-        )
-        return NextResponse.json({message:'Login successful',user:{id:user[0]._id,fullname:user[0].fullname,email:user[0].email}},{status:200})
+        // const token=jwt.sign(
+        //     {id:user[0]._id,
+        //         email:user[0].email,
+        //     },
+        //     process.env.JWT_SECRET||'secret',
+        //     {expiresIn:'1d'
+        //     }
+        // )
+        return NextResponse.json({message:'Login successful',user:{id:user[0],email:user[0].email}},{status:200})
 
     } catch(error){
+        console.error('login error:',error)
         return NextResponse.json({message:'An error occurred, please try again'},{status:500})
     }   
           
