c4cceea645912706f59c5b8f3589afecc7dc03a3
ntoiti <fidesluckia36@gmail.com>
2025-10-13 13:55:40 +0300

    added code

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -4,6 +4,10 @@
 export default function ForgotPassword() {
 
     return(
-        <div className="min-h-screen flex items-center justify-cente bg-gradient-to-br from-[#f0fdf4] to-[#dcfce7] p-4 "></div>
+        <div className="min-h-screen flex items-center justify-cente bg-gradient-to-br from-[#f0fdf4] to-[#dcfce7] p-4 ">
+            <div className="max-w-md mx-auto w-full ">
+                <div className="" ></div>
+            </div>
+        </div>
     )
 }
